0c11d03e803603b90ad97ff41b3fa26b7212f3ba
Shparvasser <Stalkerok3000@gmail.com>
2021-08-25 11:57:32 +0300

    Add db

---
+++ register.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Авг 25 2021 г., 11:56
+-- Версия сервера: 5.7.33
+-- Версия PHP: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `country`
+--
+
+CREATE TABLE `country` (
+  `id` int(11) NOT NULL,
+  `country` varchar(80) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `country`
+--
+
+INSERT INTO `country` (`id`, `country`) VALUES
+(1, 'AFGHANISTAN'),
+(2, 'ALBANIA'),
+(3, 'ALGERIA'),
+(4, 'AMERICAN SAMOA'),
+(5, 'ANDORRA'),
+(6, 'ANGOLA'),
+(7, 'ANGUILLA'),
+(8, 'ANTARCTICA'),
+(9, 'ANTIGUA AND BARBUDA'),
+(10, 'ARGENTINA'),
+(11, 'ARMENIA');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `login` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `surname` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `month` tinyint(1) UNSIGNED DEFAULT NULL,
+  `day` tinyint(1) UNSIGNED DEFAULT NULL,
+  `year` int(11) UNSIGNED DEFAULT NULL,
+  `country` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `privacy` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `registration_time` int(191) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `country`
+--
+ALTER TABLE `country`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
